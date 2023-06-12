2750da9e92349ab4b64289d49f27720b05064f8e
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-06-12 20:09:45 +0200

    update

--- unit_2/lesson_11/finish/express_templates/controllers/errorController.js
+++ unit_2/lesson_11/finish/express_templates/controllers/errorController.js
@@ -10,7 +10,10 @@ exports.logErrors = (error, req, res, next) => {
 exports.respondNoResourceFound = (req, res) => {
   let errorCode = httpStatus.NOT_FOUND;
   res.status(errorCode);
-  res.send(`${errorCode} | The page does not exist!`);
+  res.sendFile(`./public/${errorCode}.html`, {
+    root: "./"
+  });
+  //res.send(`${errorCode} | The page does not exist!`);
 };
 
 exports.respondInternalError = (error, req, res, next) => {
