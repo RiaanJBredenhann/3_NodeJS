347ef81bd4779073b5deefc5aeb3144b2ca8468b
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-06-29 13:48:51 +0200

    update

--- unit_4/lesson_20/finish/recipe_app_20_3/repl.js
+++ unit_4/lesson_20/finish/recipe_app_20_3/repl.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose"),
   Course = require("./models/course");
 var testCourse, testSubscriber;
 mongoose.connect(
-  "mongodb://localhost:27017/recipe_db",
+  "mongodb://127.0.0.1/recipe_db",
   { useNewUrlParser: true }
 );
 mongoose.set("useCreateIndex", true);
--- unit_4/lesson_21/finish/confetti_cuisine/main.js
+++ unit_4/lesson_21/finish/confetti_cuisine/main.js
@@ -13,7 +13,7 @@ const express = require("express"),
   methodOverride = require("method-override");
 
 mongoose.connect(
-  "mongodb://localhost:27017/confetti_cuisine",
+  "mongodb://127.0.0.1/confetti_cuisine",
   { useNewUrlParser: true }
 );
 mongoose.set("useCreateIndex", true);
