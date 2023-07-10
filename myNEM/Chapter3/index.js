6d9516881504fc47119b63195771973989a4de0b
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-07-10 15:39:16 +0200

    update to chapter 3

--- myNEM/Chapter3/index.js
+++ myNEM/Chapter3/index.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
 const app = new express();
+
 app.use(express.static("public"));
+
+// we tell Express to use EJS as our templating engine, that any file
+// ending in .ejs should be rendered with the EJS package
+app.set("view engine", "ejs");
 
 app.listen(3000, () => {
     console.log("App listening on port 3000")
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public/index.html"))
+    res.render("index");
 });
 
-app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'public/about.html'))
-})
+app.get('/about', (req,res)=>{
+    res.render("about");
+});
 
-app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'public/contact.html'))
-})
+app.get('/contact', (req,res)=>{
+    res.render("contact");
+});
 
-app.get('/post',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'public/post.html'))
-})
+app.get('/post', (req,res)=>{
+    res.render("post");
+});
