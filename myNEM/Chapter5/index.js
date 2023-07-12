cf1408f5efefaa83965d51e7e4e1769f2c051420
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-07-12 16:55:04 +0200

    added models to app

--- myNEM/Chapter5/index.js
+++ myNEM/Chapter5/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
 const app = new express();
+// mongoose is a package that alloes us to communicate with our database
+const mongoose = require("mongoose");
 
 app.use(express.static("public"));
+// we connect to our database with mongoose.connect
+mongoose.connect("mongodb://localhost/my_database", {useNewUserParser: true});
 
 // we tell Express to use EJS as our templating engine, that any file
 // ending in .ejs should be rendered with the EJS package
---
+++ myNEM/Chapter5/models/BlogPost.js
@@ -0,0 +1,17 @@
+// models are defined through a Schema interface
+// models are objects that represent collections in our database
+// a schema represents how a collection looks
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BlogPostSchema = new Schema({
+    title: String,
+    body: String
+});
+
+// we access the database with mongoose.model
+// mongoose looks for a collection called BlogPosts to apply the model to
+const BlogPost = mongoose.model("BlogPost", BlogPostSchema);
+// allows the use of this model in other parts of the project
+module.exports = BlogPost;
