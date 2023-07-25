362c5d243baf5f77e0f3db0b8d93e5bac6d39654
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-07-25 17:22:47 +0200

    update

--- Projects/myNEM/Chapter12/controllers/storeUser.js
+++ Projects/myNEM/Chapter12/controllers/storeUser.js
@@ -1,16 +1,29 @@
-const User = require('../models/User.js');
+const User = require('../models/User');
 const path = require('path');
 
 module.exports = (req, res) => {
-    User.create(req.body, (error, user) => {
-        if (error) {
-            // we map through the error.errors arraa keys and for each of them,
-            // access the key's error message property
-            // we also make the messages available to the view by assigning it to a variable
-            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
-            req.session.validationErrors = validationErrors;
-            return res.redirect('/auth/register/');
-        }
-        res.redirect('/')
-    })
+    User.create(req.body)
+    .then(res.redirect('/'))
+    .catch(error => {
+        // we map through the error.errors area keys and for each of them,
+        // access the key's error message property
+        // we also make the messages available to the view by assigning it to a variable
+        const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
+        req.session.validationErrors = validationErrors;
+        return res.redirect('/auth/register');
+    });
 }
+
+// module.exports = (req, res) => {
+//     User.create(req.body, (error, user) => {
+//         if (error) {
+//             // we map through the error.errors arraa keys and for each of them,
+//             // access the key's error message property
+//             // we also make the messages available to the view by assigning it to a variable
+//             const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
+//             req.session.validationErrors = validationErrors;
+//             return res.redirect('/auth/register');
+//         }
+//         res.redirect('/')
+//     })
+// }
--- Projects/myNEM/Chapter12/middleware/authMiddleware.js
+++ Projects/myNEM/Chapter12/middleware/authMiddleware.js
@@ -2,11 +2,26 @@ const User = require('../models/User');
 
 // we look for a logged in user
 // if it doesn't exist or the user is not logged in, render the home page
+
 module.exports = (req, res, next) => {
-    User.findById(req.session.userId, (error, user) => {
+    User.findById(req.session.userId)
+    .then(user => {
         if (error || !user) {
             return res.redirect('/');
         }
+        next()
+    })
+    .catch(error => {
+        console.log(error);
         next();
     });
 }
+
+// module.exports = (req, res, next) => {
+//     User.findById(req.session.userId, (error, user) => {
+//         if (error || !user) {
+//             return res.redirect('/');
+//         }
+//         next();
+//     });
+// }
