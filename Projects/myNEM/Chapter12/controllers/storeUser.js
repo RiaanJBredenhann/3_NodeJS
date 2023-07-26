4ad83c0ad409dccd7d54b46dd1acea7f06bab57a
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-07-26 13:08:39 +0200

    update

--- Projects/myNEM/Chapter12/controllers/storeUser.js
+++ Projects/myNEM/Chapter12/controllers/storeUser.js
@@ -4,6 +4,10 @@ const path = require('path');
 module.exports = (req, res) => {
     User.create(req.body, (error, user) => {
         if (error) {
+            // we map through the error.errors array keys and for each of them,
+            // access the key's error message property
+            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
+            req.session.validationErrors = validationErrors;
             console.log(error);
             return res.redirect('/auth/register/');
         }
--- Projects/myNEM/Chapter12/models/User.js
+++ Projects/myNEM/Chapter12/models/User.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
+var uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = new Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, "Please provide a Username"],
         unique: true
     },
     password: {
         type: String,
-        required: true
+        required: [true, "Please provide a Password"]
     }
 });
+
+// validator will check for duplicate database entries 
+// and report them like any other validation error
+UserSchema.plugin(uniqueValidator);
 
 // we tell mongoose that before we save any record into the Users Schema or collection,
 // we first have to excecute the 2nd argument
